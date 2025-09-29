f80c00ba6196a3d7dc66a8d0535e54babda9f411
Jérémy Ferrante <jeremyf@ferops.tech>
2025-09-29 19:39:08 +0200

    First commit of my K8s setup

--- app/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from prometheus_flask_exporter import PrometheusMetrics
-
-app = Flask(__name__)
-metrics = PrometheusMetrics(app)  # Auto /metrics + request tracking
-
-@app.route('/')
-def hello_geek():
-    return '<h1>🚀 Hello from your DevOps Playground!</h1>'
---
+++ devops-demo-container-deploy/app/app.py
@@ -0,0 +1,15 @@
+
+import osfrom flask import Flask
+from prometheus_flask_exporter import PrometheusMetrics
+
+app = Flask(__name__)
+metrics = PrometheusMetrics(app)  # Auto /metrics + request tracking
+
+@app.route('/')
+def hello_world():
+    version = os.getenv("APP_VERSION")  # no default
+    if version:
+        version_text = f" Version {version}"
+    else:
+        version_text = ""
+    return f'<h1>🚀 Hello from your DevOps Playground!{version_text}</h1>'
