0981d065101731320722b4b2bd84f0a2d28dec90
Jérémy Ferrante <jeremyf@ferops.tech>
2025-09-30 09:02:08 +0200

    Updated the app to integrate version number

--- app/app.py
+++ app/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from prometheus_flask_exporter import PrometheusMetrics
 
@@ -5,5 +6,10 @@ app = Flask(__name__)
 metrics = PrometheusMetrics(app)  # Auto /metrics + request tracking
 
 @app.route('/')
-def hello_geek():
-    return '<h1>🚀 Hello from your DevOps Playground!</h1>'
+def hello_world():
+    version = os.getenv("APP_VERSION")  # no default
+    if version:
+        version_text = f" Version {version}"
+    else:
+        version_text = ""
+    return f'<h1>🚀 Hello from your DevOps Playground!{version_text}</h1>'
